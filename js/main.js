ea51b3c78c4ed987c92310205ba18aaad030d74b
Ali503-7 <alimoali354@gmail.com>
2022-12-23 00:34:10 +0200

    done

--- js/main.js
+++ js/main.js
@@ -20,8 +20,37 @@ var splide = new Splide( '.splide', {
   type   : 'loop',
   focus: 'center',
   maxWidth: '100%',
-  mediaQuery: 'min',
   autoplay: true,
 } );
 
 splide.mount();
+
+// email
+
+let input = document.querySelector("input")
+let button = document.querySelector("footer button")
+let f_p = document.querySelector("footer .input span")
+console.log(f_p)
+function T_show() {
+  f_p.classList.add("alert-show")
+};
+function F_show() {
+  f_p.classList.remove("alert-show")
+};
+
+function ValidateEmail(mail) {
+  var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!input.value.match(regex)) {
+    T_show()
+    setTimeout(() => {
+      F_show()
+    }, 5000);
+  } else {
+    input.value = ""
+  }
+}
+
+button.onclick = () => {
+  ValidateEmail(input.value)
+  console.log('nani')
+}
